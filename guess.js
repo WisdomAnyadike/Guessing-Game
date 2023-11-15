f8f53359e5ca0b2df0d28ee46d9fadde19d5d54c
WisdomAnyadike <anyadikewisdom@icloud.com>
2023-11-15 11:48:03 +0100

    second deploy

--- guess.js
+++ guess.js
@@ -3,22 +3,44 @@ let inputValue = document.getElementById('guess')
 let text = document.getElementById('middle')
 let container = document.getElementById('container')
 let legend = document.getElementById('legend')
+let score = document.getElementById('score')
+let del = document.getElementById('del')
+let fig = 0
 
+
+if(inputValue.value == ''){
+    text.innerHTML = 'Pick a Num from 0-10'
+}
+
+
+
+ 
 
 let  guessNum = ()=> {
+
 let random_number = Math.floor(Math.random() * 10) 
-inputValue.value == random_number ?   text.innerHTML = `You are correct` : text.innerHTML = `You are not correct, the answer is ${random_number}`
+score.innerHTML = ` ${fig}`
+
+
+inputValue.value == random_number ?   text.innerHTML = `Correct Answer ${random_number} , You WON! ` : text.innerHTML = `You are Wrong, the answer is ${random_number}`
+if(text.innerHTML == `Correct Answer ${random_number} , You WON! `) { score.innerHTML = ` ${++fig}` }  
+
+
 }
 
 let  guess = (num) => {
     inputValue.value = num
 }
 
-let Del = () =>  {
-   text.innerHTML  = ''
-    inputValue.value = inputValue.value.slice(0,0)
-}
 
+let Del = () =>  {
+    fig = 0
+    text.innerHTML  = ''
+    score.innerHTML = ` ${fig}`
+     inputValue.value = inputValue.value.slice(0,0)
+     text.innerHTML = 'Pick a Num from 0-9'
+  
+ }
 
 
 
